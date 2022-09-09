c5c54fa5b1c2d6a198e1e987c87282a196cdb69b
lesterEnrique07 <lesterenriquep7@gmail.com>
2022-09-09 00:08:06 -0400

    Mostrar Destino

--- src/visual/VentanaPrincipal.java
+++ src/visual/VentanaPrincipal.java
@@ -68,6 +68,9 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                     if(subMenuIndex == 0){
                         mainForm.showForm(new ClienteForm());
                     }
+                     if(subMenuIndex == 1){
+                        mainForm.showForm(new DestinoForm());
+                    }
                     if(subMenuIndex == 2){
                         mainForm.showForm(new EjercicioForm());
                     }
