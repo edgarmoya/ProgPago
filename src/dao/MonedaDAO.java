c4f6fd1de74ad5e117d72b2d7c227d5fbb926166
lesterEnrique07 <lesterenriquep7@gmail.com>
2022-09-25 17:45:10 -0400

    Moneda con el lapicito hecho

--- src/dao/MonedaDAO.java
+++ src/dao/MonedaDAO.java
@@ -71,5 +71,61 @@ public class MonedaDAO {
         }
         return monedas;
     }
+    
+    // Obtener datos de la moneda a partir de la siglas
+    public Moneda getMoneda(String siglas) throws SQLException, ClassNotFoundException, ConnectionException, BDException {
+        Connection conn = pg.getConnection();
+        Moneda m = new Moneda();
+        if (conn == null) {
+            throw new ConnectionException("No se pudo establecer conexión con la base de datos");
+        } else {
+            try {
+                String sql = "SELECT * FROM moneda WHERE siglas = ?";
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                stmt.setString(1, siglas);
+                ResultSet rs = stmt.executeQuery();
+
+                while (rs.next()) {
+                    //Preparar los datos
+                    m.setSiglas(rs.getString("siglas"));
+                    m.setNombre(rs.getString("nombre"));
+                }
+            } catch (Exception e) {
+                System.out.println("Error al obtener moneda: " + e.getMessage());
+                throw new BDException(e.getMessage());     
+            } finally {
+                conn.close();
+            }
+        }
+        return m;
+    }
+    
+    // Actualizar moneda a partir de la sigla
+    public boolean actualizarMoneda(String sig, Moneda moneda) throws SQLException, ClassNotFoundException, ConnectionException, BDException {
+        Connection conn = pg.getConnection();
+        boolean isUpdate = false;
+        if (conn == null) {
+            throw new ConnectionException("No se pudo establecer conexión con la base de datos");
+        } else {
+            try {
+                String sql = "UPDATE moneda SET siglas=? ,nombre=? WHERE siglas=?";
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                stmt.setString(1, moneda.getSiglas());
+                stmt.setString(2, moneda.getNombre());
+                stmt.setString(3, sig);
+
+                //ejecutamos la sentencia
+                int cantidad = stmt.executeUpdate();
+                isUpdate = (cantidad > 0);
+
+            } catch (Exception e) {
+                System.out.println("Error al actualizar moneda " + e.getMessage());
+                throw new BDException(e.getMessage());             
+            } finally {
+                conn.close();
+            }
+        }
+        return isUpdate;
+    }
 }
 
